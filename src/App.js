f597536ffc8636e96ed0f317dc074985303da7cc
Ram-Surampudi <ramsurampudi20@gmail.com>
2024-06-29 18:55:58 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React from "react";
 import './Files/css/General.css';
 import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
@@ -18,12 +18,13 @@ function App() {
       <Nav/>
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Navigate to='/home' />} />
+        <Route path='/home' element={<Home/>} />
         <Route path='/ltps' element={<Ltps/>} />
         <Route path='/expected-percentage' element={<ExpectedPer/>} />
         <Route path='/attendence' element={<GeneralAttendence/>} />
         <Route path='/take-a-leave' element={<DecreaseAtt/>} />
-        <Route path='/*' element={<Navigate to='/'/>} />
+        <Route path='/*' element={<Navigate to='/home'/>} />
         </Routes>
       </BrowserRouter>
     </div>
